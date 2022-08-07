6cebc96005220d50733bcd636a5eeeb2655c92f8
lida-skakun <kunska@gmail.com>
2022-08-07 13:34:29 +0200

    Format data and time function added

---
+++ src/main.js
@@ -0,0 +1,26 @@
+function formatDate() {
+  let dateTime = document.querySelector("#dateTime");
+  let currentDate = new Date();
+
+  let day = currentDate.getDate();
+  if (day < 10) {
+    day = `0${day}`;
+  }
+
+  let month = currentDate.getMonth() + 1;
+  if (month < 10) {
+    month = "0" + month;
+  }
+
+  let year = currentDate.getFullYear();
+
+  let hours = currentDate.getHours();
+  let minutes = currentDate.getMinutes();
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  dateTime.innerHTML = `${day}.${month}.${year}  ${hours}:${minutes}`;
+}
+
+formatDate();
