7ea9f9003c6d469a660d48491eb9864e9eacfa06
lida-skakun <kunska@gmail.com>
2022-08-14 14:50:05 +0200

    Changed the displaying weekly forecast  function depending on the api call

--- src/main.js
+++ src/main.js
@@ -28,13 +28,36 @@ formatDate();
 let apiKey = "93791ed1c5ac3002a2880b95c37460d5";
 let apiEndpoint = `https://api.openweathermap.org/data/2.5/weather?`;
 
-function showForecast(forecast) {}
+function displayWeeklyForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#weekForecast");
+  let forecastHtml = `<div class="weekForecast">`;
+
+  forecast.forEach(function (day, index) {
+    if (index < 8) {
+      forecastHtml =
+        forecastHtml +
+        `<div class="row dayForecast">
+                <div class="col-8">${day.dt}</div>
+                <div class="col-2">${Math.round(day.temp.max)}</div>
+                <div class="col-2 nightTemperature">${Math.round(
+                  day.temp.max
+                )}</div>
+             </div>
+              `;
+    }
+  });
+  forecastHtml = forecastHtml + `</div>`;
+  forecastElement.innerHTML = forecastHtml;
+}
+
 function searchWeeklyForecast(coordinates) {
   let latitude = coordinates.lat;
   let longitude = coordinates.lon;
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
-  axios.get(apiUrl).then(showForecast);
+  axios.get(apiUrl).then(displayWeeklyForecast);
 }
 
 function showWeather(response) {
@@ -87,33 +110,6 @@ function getCoordinates() {
 
 let locationButton = document.querySelector("#locationButton");
 locationButton.addEventListener("click", getCoordinates);
-
-let days = [
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-  "Sunday",
-];
-
-function displayWeeklyForecast() {
-  let forecastElement = document.querySelector("#weekForecast");
-  let forecastHtml = `<div class="weekForecast">`;
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `<div class="row dayForecast">
-                <div class="col-8">${day}</div>
-                <div class="col-2">28</div>
-                <div class="col-2 nightTemperature">13</div>
-             </div>
-              `;
-  });
-  forecastHtml = forecastHtml + `</div>`;
-  forecastElement.innerHTML = forecastHtml;
-}
 
 displayWeeklyForecast();
 searchWeather("Chernihiv");
